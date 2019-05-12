81d90cefeb3bc858c7201fb98409c509deac9d63
Jorgeflowers18 <jefo20156@gmail.com>
2019-05-12 10:31:30 -0500

    Proyecto Cálculo última versión

---
+++ ProyectoCalculo/src/proyectocalculo/Proyectocalculo.java
@@ -0,0 +1,351 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocalculo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jorgeso
+ */
+public class Proyectocalculo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int opcion;
+        double x, x2, y, y2;
+
+        System.out.println(" __________________________________________________"
+                + "_______________________" + "\n"
+                + "|                                                           "
+                + "              | " + "\n"
+                + "|                          PROYECTO DE CÃ�LCULO              "
+                + "              | " + "\n"
+                + "|___________________________________________________________"
+                + "______________| " + "\n"
+                + "| Hola usuario, este es un proyecto de programacion bÃ¡sica, "
+                + "el cual te    | " + "\n"
+                + "| permitirÃ¡ encontrar los valores de la funcion F(x), solame"
+                + "nte de forma  | " + "\n"
+                + "| analÃ­tica de algunas funciones ya preleccionadas.         "
+                + "              | " + "\n"
+                + "|                                                           "
+                + "              | " + "\n"
+                + "| Funciones a expresar:                                     "
+                + "              | " + "\n"
+                + "| 1) f(x)=10x^2-x+2                                         "
+                + "              | " + "\n"
+                + "| 2) f(x)=7x^2-5x+15                                        "
+                + "              | " + "\n"
+                + "| 3) f(x)=3x^2-âˆš3                                           "
+                + "              | " + "\n"
+                + "| 4) f(x)=(x^2+2)/(-8x+3)                                   "
+                + "              | " + "\n"
+                + "| 5) f(x)=7/2 x^3+ã€–8xã€—^2-3x+2                              "
+                + "              | " + "\n"
+                + "| 6) f(x)=(x^2-5)/2x                                        "
+                + "              | " + "\n"
+                + "| 7) f(x)=4x^3-8x+1                                         "
+                + "              | " + "\n"
+                + "| 8) f(x)=15x^3+2x^2+1                                      "
+                + "              | " + "\n"
+                + "| 9) f(x)=3x^2+2x+5                                         "
+                + "              | " + "\n"
+                + "| 10) f(x)=2x^2-x+10                                        "
+                + "              | " + "\n"             
+                + "|___________________________________________________________"
+                + "______________| ");
+        System.out.print("Ingrese el nÃºmero de la funcion que desea realizar "
+                + "(1-10): ");
+        opcion = entrada.nextInt();
+        System.out.print("Ingrese el valor intermedio de X: ");
+        x = entrada.nextDouble();
+        System.out.println("|__________________________________________________"
+                + "_______________________");
+
+        x2 = x - 1;
+
+        // Se verifica la opciÃ³n acerca de la funcion escogida por el usuario
+        switch (opcion) {
+            case 1:
+                System.out.println(" FunciÃ³n: f(x)=10x^2-x+2");
+                System.out.println(" __________________________" + "\n"
+                        + "|   Valor X  |  Valor f(X) |" + "\n"
+                        + "|__________________________|" + "\n"
+                        + "|      Mayores que X       |" + "\n"
+                        + "|__________________________|");
+
+                // Determinacion de valores en un rango 
+                // 10 nÃºmero Igual y Mayores que X
+                for (int i = 0; i < 10; i++) {
+                    y = 10 * (x * x) - (x) + 2;
+                    System.out.println("|     " + x + " " + "       " + y);
+                    x = x + 1;
+                }
+                System.out.println("|__________________________ " + "\n"
+                        + "|       Menores que X      |" + "\n"
+                        + "|__________________________|");
+                // 10 nÃºmeros Menores que X
+                for (int i = 0; i < 10; i++) {
+                    y = 10 * (x2 * x2) - (x2) + 2;
+                    System.out.println("|    " + x2 + " " + "       " + y);
+                    x2 = x2 - 1;
+                }
+                break;
+
+            case 2:
+                System.out.println(" FunciÃ³n: f(x)=7x^2-5x+15");
+                System.out.println(" __________________________" + "\n"
+                        + "|   Valor X  |  Valor f(X) |" + "\n"
+                        + "|__________________________|" + "\n"
+                        + "|      Mayores que X       |" + "\n"
+                        + "|__________________________|");
+                // Determinacion de valores en un rango 
+                // 10 nÃºmero Igual y Mayores que X
+                for (int i = 0; i < 10; i++) {
+                    y = 7 * (x * x) - 5 * (x) + 15;
+                    System.out.println("|     " + x + " " + "       " + y);
+                    x = x + 1;
+                }
+                System.out.println("|__________________________ " + "\n"
+                        + "|       Menores que X      |" + "\n"
+                        + "|__________________________|");
+                // 10 nÃºmeros Menores que X
+                for (int i = 0; i < 10; i++) {
+                    y = 7 * (x2 * x2) - 5 * (x2) + 15;
+                    System.out.println("|    " + x2 + " " + "       " + y);
+                    x2 = x2 - 1;
+                }
+                break;
+
+            case 3:
+                System.out.println(" FunciÃ³n: f(x)=3x^2-âˆš3");
+                System.out.println(" __________________________" + "\n"
+                        + "|   Valor X  |  Valor f(X) |" + "\n"
+                        + "|__________________________|" + "\n"
+                        + "|      Mayores que X       |" + "\n"
+                        + "|__________________________|");
+                // Determinacion de valores en un rango 
+                // 10 nÃºmero Igual y Mayores que X
+                for (int i = 0; i < 10; i++) {
+                    y = 3 * (x * x) - Math.sqrt(3);
+                    System.out.println("|     " + x + " " + "       " + y);
+                    x = x + 1;
+                }
+                System.out.println("|__________________________ " + "\n"
+                        + "|       Menores que X      |" + "\n"
+                        + "|__________________________|");
+                // 10 nÃºmeros Menores que X
+                for (int i = 0; i < 10; i++) {
+                    y = 3 * (x2 * x2) - Math.sqrt(3);
+                    System.out.println("|    " + x2 + " " + "       " + y);
+                    x2 = x2 - 1;
+                }
+                break;
+
+            case 4:
+                System.out.println(" FunciÃ³n: f(x)=(x^2+2)/(-8x+3)");
+                System.out.println(" __________________________" + "\n"
+                        + "|   Valor X  |  Valor f(X) |" + "\n"
+                        + "|__________________________|" + "\n"
+                        + "|      Mayores que X       |" + "\n"
+                        + "|__________________________|");
+
+                // 10 nÃºmero Igual y Mayores que X
+                for (int i = 0; i < 10; i++) {
+                    y = ((x * x) + 2) / (-8 * x + 3);
+                    System.out.println("|     " + x + " " + "       " + y);
+                    x = x + 1;
+                }
+                System.out.println("|__________________________ " + "\n"
+                        + "|       Menores que X      |" + "\n"
+                        + "|__________________________|");
+                // 10 nÃºmeros Menores que X
+                for (int i = 0; i < 10; i++) {
+                    y = ((x2 * x2) + 2) / (-8 * x2 + 3);
+                    System.out.println("|    " + x2 + " " + "       " + y);
+                    x2 = x2 - 1;
+                }
+                break;
+
+            case 5:
+                System.out.println(" FunciÃ³n: f(x)=7/2 x^3+ã€–8xã€—^2-3x+2 ");
+                System.out.println(" __________________________" + "\n"
+                        + "|   Valor X  |  Valor f(X) |" + "\n"
+                        + "|__________________________|" + "\n"
+                        + "|      Mayores que X       |" + "\n"
+                        + "|__________________________|");
+
+                // Determinacion de valores en un rango 
+                // 10 nÃºmero Igual y Mayores que X
+                for (int i = 0; i < 10; i++) {
+                    y = (3.5 * (x * x * x))+ (8 *(x*x))-(3 * x) + 2;
+                    System.out.println("|     " + x + " " + "       " + y);
+                    x = x + 1;
+                }
+                System.out.println("|__________________________ " + "\n"
+                        + "|       Menores que X      |" + "\n"
+                        + "|__________________________|");
+                // 10 nÃºmeros Menores que X
+                for (int i = 0; i < 10; i++) {
+                    y = (3.5 * (x2 * x2 * x2))+ (8 *(x2*x2))-(3 * x2) + 2;
+                    System.out.println("|    " + x2 + " " + "       " + y);
+                    x2 = x2 - 1;
+                }
+                break;
+
+            case 6:
+                System.out.println(" FunciÃƒÂ³n: f(x)=(x^2-5)/2x");
+                System.out.println(" __________________________" + "\n"
+                        + "|   Valor X  |  Valor f(X) |" + "\n"
+                        + "|__________________________|" + "\n"
+                        + "|      Mayores que X       |" + "\n"
+                        + "|__________________________|");
+                // Determinacion de valores en un rango 
+                // 10 nÃƒÂºmero Igual y Mayores que X
+                for (int i = 0; i < 10; i++) {
+                    y = (x * x) - 5 / 2 * x;
+                    System.out.println("|     " + x + " " + "       " + y);
+                    x = x + 1;
+                }
+                System.out.println("|__________________________ " + "\n"
+                        + "|       Menores que X      |" + "\n"
+                        + "|__________________________|");
+                // 10 nÃƒÂºmeros Menores que X
+                for (int i = 0; i < 10; i++) {
+                    y = (x2 * x2) - 5 / 2 * x2;
+                    System.out.println("|    " + x2 + " " + "       " + y);
+                    x2 = x2 - 1;
+                }
+                break;
+
+            case 7:
+                System.out.println(" FunciÃƒÂ³n: f(x)=4x^3-8x+1");
+                System.out.println(" __________________________" + "\n"
+                        + "|   Valor X  |  Valor f(X) |" + "\n"
+                        + "|__________________________|" + "\n"
+                        + "|      Mayores que X       |" + "\n"
+                        + "|__________________________|");
+                // Determinacion de valores en un rango 
+                // 10 nÃƒÂºmero Igual y Mayores que X
+                for (int i = 0; i < 10; i++) {
+                    y = 3 * (x * x * x) - 8 * x + 1;
+                    System.out.println("|     " + x + " " + "       " + y);
+                    x = x + 1;
+                }
+                System.out.println("|__________________________ " + "\n"
+                        + "|       Menores que X      |" + "\n"
+                        + "|__________________________|");
+                // 10 nÃƒÂºmeros Menores que X
+                for (int i = 0; i < 10; i++) {
+                    y = 3 * (x2 * x2 * x2) - 8 * x2 + 1;
+                    System.out.println("|    " + x2 + " " + "       " + y);
+                    x2 = x2 - 1;
+                }
+                break;
+            case 8:
+                System.out.println(" FunciÃƒÂ³n: f(x)=15x^3+2x^2+1");
+                System.out.println(" __________________________" + "\n"
+                        + "|   Valor X  |  Valor f(X) |" + "\n"
+                        + "|__________________________|" + "\n"
+                        + "|      Mayores que X       |" + "\n"
+                        + "|__________________________|");
+                // Determinacion de valores en un rango 
+                // 10 nÃƒÂºmero Igual y Mayores que X
+                for (int i = 0; i < 10; i++) {
+                    y = 15 * (x * x * x) + 2 * (x * x) + 1;
+                    System.out.println("|     " + x + " " + "       " + y);
+                    x = x + 1;
+                }
+                System.out.println("|__________________________ " + "\n"
+                        + "|       Menores que X      |" + "\n"
+                        + "|__________________________|");
+                // 10 nÃƒÂºmeros Menores que X
+                for (int i = 0; i < 10; i++) {
+                    y = 15 * (x2 * x2 * x2) + 2 * (x2 * x2) + 1;
+                    System.out.println("|    " + x2 + " " + "       " + y);
+                    x2 = x2 - 1;
+                }
+
+                break;
+            
+            case 9:
+                
+                System.out.println(" FunciÃƒÂ³n: f(x)= 3x^2+2x+5");
+                System.out.println(" __________________________" + "\n" +
+                                   "|   Valor X  |  Valor f(X) |" + "\n" +
+                                   "|__________________________|" + "\n"+
+                                   "|      Mayores que X       |" +"\n"+
+                                   "|__________________________|");
+                // Determinacion de valores en un rango 
+                // 10 nÃƒÂºmero Igual y Mayores que X
+                for (int i = 0; i < 10 ; i++) {
+                    y = (3*(x*x))+(2*x)+5;
+                    System.out.println("|     "+x+" "+"       "+y);
+                    x = x+1;
+                }
+                System.out.println("|__________________________ " +"\n"+
+                                   "|       Menores que X      |" +"\n"+
+                                   "|__________________________|");
+                // 10 nÃƒÂºmeros Menores que X
+                for (int i = 0; i < 10; i++) {
+                    y = (3*(x2 * x2))+(2 * x2)+5;
+                    System.out.println("|    "+x2+" "+"       "+y);
+                    x2 = x2-1;}
+                
+                break;
+            case 10:
+                
+                System.out.println(" FunciÃƒÂ³n: f(x)= 2x^2-x+10");
+                System.out.println(" __________________________" + "\n" +
+                                   "|   Valor X  |  Valor f(X) |" + "\n" +
+                                   "|__________________________|" + "\n"+
+                                   "|      Mayores que X       |" +"\n"+
+                                   "|__________________________|");
+                // Determinacion de valores en un rango 
+                // 10 nÃƒÂºmero Igual y Mayores que X
+                for (int i = 0; i <10 ; i++) {
+                    y = (2 *(x * x))- x + 10;
+                    System.out.println("|     "+x+" "+"       "+y);
+                    x = x+1;
+                }
+                System.out.println("|__________________________ " +"\n"+
+                                   "|       Menores que X      |" +"\n"+
+                                   "|__________________________|");
+                // 10 nÃƒÂºmeros Menores que X
+                for (int i = 0; i < 10; i++) {
+                    y = (2 *(x2 * x2))- x2 + 10;
+                    System.out.println("|    "+x2+" "+"       "+y);
+                    x2 = x2-1;}
+                
+                break;    
+            default:
+                System.out.println("| El nÃºmero de funciÃ³n que ingresaste no "
+                        + "pertenece a ninguno              |" + "\n"
+                        + "| de los expresados anteriormente, por favor ingresa"
+                        + " un nÃºmero            |" + "\n"
+                        + "|                            _______________________"
+                        + "______________________|" + "\n"
+                        + "|    Del 1   |  Hasta el 10 |");
+
+                System.out.println("|____________|______________|");
+                System.out.println(" __________________________________________"
+                        + "__________________________" + "\n"
+                        + "| Gracias.                                          "
+                        + "               |" + "\n"
+                        + "| Creditos: Aguirre Alex, Flores Jorge, GarcÃ­a "
+                        + "Santiago, Leiva Diego |" + "\n"
+                        + "|___________________________________________________"
+                        + "_________________|");
+
+        }
+    }
+    
+}
